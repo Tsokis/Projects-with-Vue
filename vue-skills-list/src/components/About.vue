435cfd77ad5afb9ba7bb8078a8694d78c26a781d
Nikos Ntaf <nik.ntf1@gmail.com>
2018-09-05 19:15:19 +0300

    Skills List Form with Vue

---
+++ vue-skills-list/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>Hello {{ their_name }}, This is the About page</h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam consectetur, delectus optio modi praesentium architecto ipsum in repellat facere aliquid debitis harum accusamus, nulla ipsa? Explicabo, inventore? Eaque, inventore. </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "skills",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
+
